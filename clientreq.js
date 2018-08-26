f2224cf654e69f560bc629e519566a5c46751214
Tiwari-Saurabh <saurabhvut8@gmail.com>
2018-08-26 11:53:38 +0530

    First Commit

---
+++ clientreq.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    fs.readFile("summer.html", function (err, data) {
+        if (err) {
+            res.writeHead(404, {
+                'Content-Type': 'text/html'
+            });
+            return res.end("Error 404 Not Found!");
+        }
+        res.writeHead(200, {
+            'Contnt-Type': 'text/html'
+        });
+        res.write(data);
+        return res.end();
+    });
+}).listen(8080);
---
+++ first.js
@@ -0,0 +1,6 @@
+var name = "saurabh";
+            var age = 23;
+            var message = "hi,my name is " + name + " im " + age + " years old";
+//var message = 'test';      
+     
+console.log(message);
