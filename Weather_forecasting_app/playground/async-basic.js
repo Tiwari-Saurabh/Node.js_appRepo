6c8647cf39b3c6085b9e3d7006b0694eb62d0253
Tiwari-Saurabh <saurabhvut8@gmail.com>
2018-09-05 02:13:58 +0530

    removing deleted files from tracking

--- Weather_forecasting_app/playground/async-basic.js
+++
@@ -1,9 +0,0 @@
-/*jshint esversion: 6 */
-console.log("Starting the app");
-setTimeout(() => {
-    console.log("Inside the call back");
-}, 2000);
-setTimeout(() => {
-    console.log("Second time out");
-}, 0);
-console.log("End of the program");
