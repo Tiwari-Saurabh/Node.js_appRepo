13aa9c61f74411b32b9a94b65876dd338791474d
Tiwari-Saurabh <saurabhvut8@gmail.com>
2018-09-12 23:43:41 +0530

    location updatiion

--- Weather_forecasting_app/app.js
+++ Weather_forecasting_app/app.js
@@ -1,10 +1,16 @@
 /*jshint esversion: 6 */
-const request = require('request');
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
-request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=Sangli',
+const request = require("request");
+//process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+request(
+  {
+    //"https://maps.googleapis.com/maps/api/geocode/json?address=Sangli",
+    url:
+      "http://www.mapquestapi.com/geocoding/v1/address?key=oYpt31O9Vu5j0oOFi7bv1FMDUyUTnCNw&location=sangli",
     json: true
-}, (error, response, body) => {
-    console.log(body);
-    console.log(error);
-});
+  },
+  (error, response, body) => {
+    //console.log(JSON.stringify(response, undefined, 2));
+    console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
+    console.log(`Longitude:${body.results[0].locations[0].latLng.lng}`);
+  }
+);
