709a08cc3e5c70ff8bba364c1623089efc01152b
Tiwari-Saurabh <saurabhvut8@gmail.com>
2018-10-11 01:44:29 +0530

    basic server functions

---
+++ Node-WebServer/server.js
@@ -0,0 +1,26 @@
+/*jshint esversion: 6 */
+const express = require('express');
+
+var app = express();
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello Express!</h1>');
+    res.send({
+        name: 'Saurabh',
+        Age: 23,
+        Hobbies: [
+            'Cricket',
+            'reading',
+            'Gaming'
+        ]
+    });
+});
+app.get('/about', (req, res) => {
+    res.send('<h1>About Page</h1>')
+});
+app.get('/bad', (req, res) => {
+    res.send({
+        status: 'Unable to handle request'
+    });
+});
+
+app.listen(3000);
