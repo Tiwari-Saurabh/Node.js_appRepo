ac7c17014b79da65f15bb8de8a957c41fdbd8d5f
Tiwari-Saurabh <saurabhvut8@gmail.com>
2018-10-12 01:00:13 +0530

    handlebar function initilize

--- Node-WebServer/server.js
+++ Node-WebServer/server.js
@@ -1,7 +1,13 @@
 /*jshint esversion: 6 */
 const express = require('express');
+const hbs = require('hbs');
+
 
 var app = express();
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
     // res.send('<h1>Hello Express!</h1>');
     res.send({
@@ -15,10 +21,13 @@ app.get('/', (req, res) => {
     });
 });
 
-app.use(express.static(__dirname + '/public'));
+
 
 app.get('/about', (req, res) => {
-    res.send('<h1>About Page</h1>');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentDate: new Date().getFullYear()
+    });
 });
 app.get('/bad', (req, res) => {
     res.send({
@@ -26,4 +35,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
