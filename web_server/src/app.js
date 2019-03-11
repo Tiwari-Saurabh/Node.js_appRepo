5b206d1f35f342158aff42844889172c3a782340
Tiwari-Saurabh <saurabhvut8@gmail.com>
2019-03-11 16:49:56 +0530

    Express server

---
+++ web_server/src/app.js
@@ -0,0 +1,34 @@
+/*jshint esversion: 6 */
+
+const path = require('path');
+const express = require('express');
+// console.log(__dirname);
+// console.log(path.join(__dirname , '../public'));
+const app = express();
+const publicDirectoryPath = path.join(__dirname , '../public') ;  //to access index page i.e is home page
+
+app.use(express.static(publicDirectoryPath));
+
+
+// app.get('' , (req , res)=>{
+//     res.send('<h1> Welcome </h1>');
+// });
+
+app.get('/help' , (req , res)=>{
+    res.send('Help page');
+});
+
+app.get('/about' , (req , res) =>{
+    res.send('<h1>About page</h1>');
+});
+
+app.get('/weather' , (req , res)=>{
+    res.send({
+        forecasting : ' It is Sunny today ',
+        location : 'Pune'
+    });
+});
+
+app.listen(3000 , () =>{
+    console.log('Server is running on port 3000');
+});
