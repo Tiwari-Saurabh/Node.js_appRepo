807e1686c353f74d192c2de684c3dcbc2429d94d
Tiwari-Saurabh <saurabhvut8@gmail.com>
2019-03-11 18:04:09 +0530

    initialized js file to index .html

--- web_server/src/app.js
+++ web_server/src/app.js
@@ -14,13 +14,13 @@ app.use(express.static(publicDirectoryPath));
 //     res.send('<h1> Welcome </h1>');
 // });
 
-app.get('/help' , (req , res)=>{
-    res.send('Help page');
-});
+// app.get('/help' , (req , res)=>{
+//     res.send('Help page');
+// });
 
-app.get('/about' , (req , res) =>{
-    res.send('<h1>About page</h1>');
-});
+// app.get('/about' , (req , res) =>{
+//     res.send('<h1>About page</h1>');
+// });
 
 app.get('/weather' , (req , res)=>{
     res.send({
