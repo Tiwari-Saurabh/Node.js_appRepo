300cc3b5802e69f07427a0e8c4c7f9ecad45cca4
Tiwari-Saurabh <saurabhvut8@gmail.com>
2019-03-12 15:13:17 +0530

    index.hbs implemented

--- web_server/src/app.js
+++ web_server/src/app.js
@@ -5,9 +5,9 @@ const express = require('express');
 // console.log(__dirname);
 // console.log(path.join(__dirname , '../public'));
 const app = express();
-const publicDirectoryPath = path.join(__dirname , '../public') ;  //to access index page i.e is home page
+const publicDirectoryPath = path.join(__dirname, '../public'); //to access index page i.e is home page
 
-app.set('view engine' , 'hbs');
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
 
 
@@ -22,14 +22,17 @@ app.use(express.static(publicDirectoryPath));
 // app.get('/about' , (req , res) =>{
 //     res.send('<h1>About page</h1>');
 // });
+app.get('', (req, res) => {
+    res.render('index');
+});
 
-app.get('/weather' , (req , res)=>{
+app.get('/weather', (req, res) => {
     res.send({
-        forecasting : ' It is Sunny today ',
-        location : 'Pune'
+        forecasting: ' It is Sunny today ',
+        location: 'Pune'
     });
 });
 
-app.listen(3000 , () =>{
+app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
