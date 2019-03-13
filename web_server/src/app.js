efc5227792835f4f8784265cddd0a1c0936fc936
Tiwari-Saurabh <saurabhvut8@gmail.com>
2019-03-13 10:24:37 +0530

    customized views to templete

--- web_server/src/app.js
+++ web_server/src/app.js
@@ -1,15 +1,16 @@
 /*jshint esversion: 6 */
 
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 // console.log(__dirname);
 // console.log(path.join(__dirname , '../public'));
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public'); //to access index page i.e is home page
+const publicDirectoryPath = path.join(__dirname, "../public"); //to access index page i.e is home page
+const viewsPath = path.join(__dirname, "../templetes");
 
-app.set('view engine', 'hbs');
+app.set("view engine", "hbs");
+app.set("views", "viewsPath");
 app.use(express.static(publicDirectoryPath));
-
 
 // app.get('' , (req , res)=>{
 //     res.send('<h1> Welcome </h1>');
@@ -22,27 +23,34 @@ app.use(express.static(publicDirectoryPath));
 // app.get('/about' , (req , res) =>{
 //     res.send('<h1>About page</h1>');
 // });
-app.get('', (req, res) => {
-    res.render('index', {
-        title: 'Weather Application',
-        name: 'Saurabh Tiwari'
-    });
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather Application",
+    name: "Saurabh Tiwari"
+  });
 });
 
-app.get('/about', (re, res) => {
-    res.render('about', {
-        title: 'About me',
-        name: 'Saurabh Tiwari'
-    });
+app.get("/about", (re, res) => {
+  res.render("about", {
+    title: "About me",
+    name: "Saurabh Tiwari"
+  });
 });
 
-app.get('/weather', (req, res) => {
-    res.send({
-        forecasting: ' It is Sunny today ',
-        location: 'Pune'
-    });
+app.get("/help", (re, res) => {
+  res.render("help", {
+    title: "Help",
+    name: "Saurabh Tiwari"
+  });
+});
+
+app.get("/weather", (req, res) => {
+  res.send({
+    forecasting: " It is Sunny today ",
+    location: "Pune"
+  });
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
