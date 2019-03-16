78517097d9f3a3de82dd96ebd7c614bfaf20756d
Tiwari-Saurabh <saurabhvut8@gmail.com>
2019-03-16 16:36:23 +0530

    fetch function initialized to fetch weather info

--- web_server/public/js/app.js
+++ web_server/public/js/app.js
@@ -1,1 +1,19 @@
+/*jshint esversion: 6 */
 console.log("Something for later");
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//     response.json.then((data) => {
+//         console.log(data);
+//     });
+// });
+
+fetch('http://localhost:3000/weather?address=mumbai').then((response) => {
+    response.json.then((data) => {
+        if (data.error) {
+            console.log(data.error, "Unable to fetch the data");
+        } else {
+            console.log(data.forecast);
+            console.lof(data.location);
+        }
+    });
+
+});
